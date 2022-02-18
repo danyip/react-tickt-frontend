0e280dd1f6b7dc5b5b216b429c5098334ea0c69b
Steven Furnari <s.furnari@hotmail.com>
2022-02-18 23:54:34 +1100

    frontend user login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios'
+import {Route, Link, HashRouter as Router} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Login from './components/Login'
+import MyProfile from './components/MyProfile'
+
+const BASE_URL = 'http://localhost:3000'
+
+class App extends React.Component{
+
+  //App state
+  state = {
+    currentUser: undefined
+  }
+
+  //function to run on component mounting
+  componentDidMount(){
+    this.setCurrentUser();
+  }
+
+  //function to set the state to the current logged in user
+  setCurrentUser = () => {
+    let token = "Bearer " + localStorage.getItem("jwt");
+    axios.get(`${BASE_URL}/users/current`, {
+      headers: {
+        'Authorization': token
+      }
+    })
+    .then(res => {
+      this.setState({currentUser: res.data})
+    })
+    .catch(err => console.warn(err))
+  }
+
+  //function to log the user out.
+  handleLogout = () => {
+    this.setState({currentUser: undefined})
+    localStorage.removeItem("jwt");
+    axios.defaults.headers.common['Authorization'] = undefined;
+  }
+
+  render(){
+    return (
+      <Router>
+        <header>
+          <nav>
+            {
+              this.state.currentUser !== undefined
+              ?
+              (
+                <ul>
+                  <li>Welcome {this.state.currentUser.name} | </li>
+                  <li><Link to='/my_profile'>My Profile</Link></li>
+                  <li><Link onClick={this.handleLogout} to='/'>Logout</Link></li>
+                </ul>
+              )
+              :
+              (
+                <ul>
+                  <li><Link to='/login'>Login</Link></li>
+                </ul>
+              )
+            }
+          </nav>
+          <hr/>
+        </header>
+        <Route exact path='/my_profile' component={MyProfile}/>
+        <Route
+          exact path='/login'
+          render={(props) => <Login setCurrentUser={this.setCurrentUser}{...props}/>}
+          />
+      </Router>
+    ); // return
+  } // render
+} //class App
 
 export default App;
