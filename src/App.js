8eb43d3f36a961422b2aaeeed3426df2c250f4f4
Cameron <reid.camerond@gmail.com>
2022-02-19 12:24:00 +1100

    all app components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {HashRouter as Router} from 'react-router-dom';
+import Header from './components/Home/Header';
+import Footer from './components/Home/Footer';
+import Routes from './Routes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+
+  }
+
+  componentDidMount(){
+
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Tickt</h1>
+
+        <Router>
+
+        </Router>
+      </div>
+    )
+  }
+  
 }
 
 export default App;
