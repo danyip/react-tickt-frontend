78fbc686ccdb607e6fd5c4ea1ca74a031ec3d4e5
danyip <danielyip90@gmail.com>
2022-02-20 12:49:45 +1100

    AllEvents API request working

---
+++ src/pages/AllEvents.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import React, { Component } from 'react'
+import { BASE_URL } from '../apiBaseUrl';
+
+export default class AllEvents extends Component {
+  state = {
+    allEvents: []
+  }
+
+  fetchAllEvents = async ()=>{
+    const url = `${BASE_URL}/events`
+
+    try {
+      const res = await axios.get(url)
+      console.log('fetchAllEvents()',res.data);
+      this.setState({allEvents: res.data})
+    } catch (err) {
+      console.log('ERROR FETCHING ALL EVENTS: ', err);
+    }
+  }
+
+  componentDidMount(){
+    this.fetchAllEvents()
+  }
+
+  handleClick = (id)=>{
+    console.log('Clicked: ', id);
+    this.props.history.push(`/event/${id}`)
+  }
+
+  render() {
+    return (
+      <div>
+        {
+          this.state.allEvents.map(event => {
+            return(<article key={event.id}>
+              <h3 onClick={()=>this.handleClick(event.id)}>{event.name}</h3>
+              <p> <strong>Venue: </strong>{event.venue.name}</p>
+              <p>{event.description}</p>
+              <hr/>
+            </article>)
+          })
+        }
+      </div>
+    )
+  }
+}
