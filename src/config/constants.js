5e9256b26f8ae6e3fed73dc56f5cf4f91933be00
Steven Furnari <s.furnari@hotmail.com>
2022-02-23 23:53:28 +1100

    added live backend url to config

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.NODE_ENV === "production" ? "livelink" : "http://localhost:3000"
+export const BASE_URL = process.env.NODE_ENV === "production" ? "https://tickt-backend.herokuapp.com/" : "http://localhost:3000"
