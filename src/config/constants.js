7f53c702f96bafa2e1068ebfeb71dbfbd1bf3812
danyip <hello@danyip.com>
2022-03-01 14:05:49 +1100

    changed prod api url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.NODE_ENV === "production" ? "https://tickt-backend.herokuapp.com/" : "http://localhost:3000"
+export const BASE_URL = process.env.NODE_ENV === "production" ? " https://desolate-badlands-87743.herokuapp.com/" : "http://localhost:3000"
